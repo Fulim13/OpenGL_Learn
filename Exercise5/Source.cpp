2d86630b2f61a5901d0fcfbe4f6215ff3dcc0551
Fu Lim <fulim1130@gmail.com>
2024-12-23 19:37:26 +0800

    Complete Bridge

--- Exercise5/Source.cpp
+++ Exercise5/Source.cpp
@@ -12,118 +12,68 @@
 float speed = 0.0, bspeed = 0.0;
 
 //projection
-float tx = 0, tz = 0, tSpeed = 0.05; //translate for modelview
-boolean isOrtho = true;              //is ortho view
-float ONear = -20, OFar = 20;        //Ortho near far
-float PNear = 1, PFar = 30;          //Perspective near far
-float ptx = 0, pty = 0;              //translate for projection
-float pry = 0, prSpeed = 1.0;        //rotate ry for projection
-float prx = 0;                       //rotate rx for projection
-//cloud controller
-boolean cloudMain = false;
-float cloudX = 0.0, cloudY = 0.0, cloudZ = 0.0;
-float cloudSpeed = 0.01;
+float tx = 0, tz = -3, tSpeed = 0.1; // Translate for modelview (x and z axis movement for camera)
+float ty = 0, ySpeed = 0.1;           // Camera up/down movement (y axis)
+float cameraSpeed = 0.1;              // General camera movement speed
+float cameraAngleX = 0, cameraAngleY = 0;  // Camera rotation (X and Y axis rotation)
+float ONear = 1, OFar = 20;         // Ortho near far
+float PNear = 1, PFar = 30;           // Perspective near far
+boolean isOrtho = true;               // Is ortho view flag
 
-LRESULT WINAPI WindowProcedure(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
-{
-	switch (msg)
-	{
+LRESULT WINAPI WindowProcedure(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+	switch (msg) {
 	case WM_DESTROY:
 		PostQuitMessage(0);
 		break;
 
 	case WM_KEYDOWN:
-		if (wParam == VK_ESCAPE)
+		if (wParam == VK_ESCAPE) {
 			PostQuitMessage(0);
-		else if (wParam == 'W') {
+		}
+		else if (wParam == 'N') {
 			speed = +0.1;
 		}
-		else if (wParam == 'S') {
+		else if (wParam == 'M') {
 			speed = -0.1;
 		}
-		else if (wParam == VK_UP) {            //Zoom In Projection
-			if (isOrtho) {
-				if (tz > ONear)
-				{
-					//Ortho view - zoom in
-					tz -= tSpeed;
-				}
-			}
-			else
-			{
-				if (tz > PNear + 1)
-				{
-					//perspective view - zoom in
-					tz -= tSpeed;
-				}
-			}
+		else if (wParam == 'W') {         // Move camera forward
+			tz -= cameraSpeed;
 		}
-		else if (wParam == VK_DOWN) {        //Zoom Out Projection
-			if (isOrtho)
-			{
-				if (tz < OFar)
-				{
-					//Ortho view - zoom out
-					tz += tSpeed;
-				}
-			}
-			else
-			{
-				//perspective view - zoom out
-				if (tz < PFar / 2)
-				{
-					tz += tSpeed;
-				}
-			}
+		else if (wParam == 'S') {         // Move camera backward
+			tz += cameraSpeed;
 		}
-		else if (wParam == 'O') {            //Change to Orthographic Projection
-			isOrtho = true;
-			tz = 0;
+		else if (wParam == VK_UP) {       // Move camera up (on Y-axis)
+			ty -= ySpeed;
 		}
-		else if (wParam == 'P') {            //Change to Perspective Projection
-			isOrtho = false;
-			tz = 2;
+		else if (wParam == VK_DOWN) {     // Move camera down (on Y-axis)
+			ty += ySpeed;
 		}
-		else if (wParam == VK_RIGHT) {        //Translate Projection to left hand side
-			tx -= tSpeed;
-		}
-		else if (wParam == VK_LEFT) {        //Translate Projection to right hand side
+		else if (wParam == VK_LEFT) {     // Move camera left (on X-axis)
 			tx += tSpeed;
 		}
-		else if (wParam == 'A') {            //Rotate Projection at x axis (anti-clockwise)
-			prx += prSpeed;
+		else if (wParam == VK_RIGHT) {    // Move camera right (on X-axis)
+			tx -= tSpeed;
 		}
-		else if (wParam == 'D') {            //Rotate Projection at x axis (clockwise)
-			prx -= prSpeed;
+		else if (wParam == 'A') {         // Rotate camera around X-axis (counterclockwise)
+			cameraAngleX += 1.0f;
 		}
-		else if (wParam == 'L') {            //Rotate Projection at y axis (anti-clockwise)
-			pry += prSpeed;
+		else if (wParam == 'D') {         // Rotate camera around X-axis (clockwise)
+			cameraAngleX -= 1.0f;
 		}
-		else if (wParam == 'R') {            //Rotate Projection at y axis (clockwise)
-			pry -= prSpeed;
+		else if (wParam == 'Q') {         // Rotate camera around Y-axis (counterclockwise)
+			cameraAngleY += 1.0f;
 		}
-		// ptx
-		else if (wParam == 'U') {            //Translate Projection to up hand side
-					ptx += tSpeed;
-				}
-		else if (wParam == 'J') {            //Translate Projection to down hand side
-					ptx -= tSpeed;
-				}
-
-		// pty
-		else if (wParam == 'I') {            //Translate Projection to up hand side
-					pty += tSpeed;
-				}
-		else if (wParam == 'K') {            //Translate Projection to down hand side
-					pty -= tSpeed;
-				}
-		else if (wParam == 'C') {
-			cloudMain = true;
-			cloudX = 0;
-			cloudY = 0;
-			cloudZ = 0;
+		else if (wParam == 'E') {         // Rotate camera around Y-axis (clockwise)
+			cameraAngleY -= 1.0f;
+		}
+		else if (wParam == 'O') {         // Switch to Orthographic projection
+			isOrtho = true;
+		}
+		else if (wParam == 'P') {         // Switch to Perspective projection
+			isOrtho = false;
 		}
 		break;
+
 	default:
 		break;
 	}
@@ -164,24 +114,20 @@ void projection() {
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 
-	if (isOrtho)
-	{
-		glRotatef(180, 0, 1, 0);
-		glOrtho(0, 0, 0, 0, ONear, OFar);
+	if (isOrtho) {
+		glOrtho(-1, 1, -1, 1, ONear, OFar);  
 	}
-	else
-	{
-		glRotatef(180, 0, 1, 0);
-		gluPerspective(20, 1, PNear, PFar);
+	else {
+		gluPerspective(45.0f, 1.0f, PNear, PFar); 
 	}
 
-	glRotatef(pry, 0, 1, 0);
-	glRotatef(prx, 1, 0, 0);
-	glTranslatef(ptx, pty, 0.0);
+	glTranslatef(tx, ty, tz);
+
+	glRotatef(cameraAngleY, 0, 1, 0);  
+	glRotatef(cameraAngleX, 1, 0, 0);  
 
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
-	glTranslatef(tx, 0, tz);
 }
 
 void drawCube(float sx, float sy, float sz) {
